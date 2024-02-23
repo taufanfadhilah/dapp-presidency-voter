b0f6eca34d9daba7ffa27ca28b4ade561281e4fa
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2024-02-23 17:23:44 +0700

    feat: add confirmation modal

--- app/vote/_components/CandidateCard.tsx
+++ app/vote/_components/CandidateCard.tsx
@@ -3,6 +3,17 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { CheckCircle, Loader2 } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
@@ -53,14 +64,33 @@ export function CandidateCard({
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="w-full" onClick={onSubmit} disabled={isLoading}>
-          {isLoading ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <CheckCircle className="mr-2 h-4 w-4" />
-          )}{" "}
-          Choose
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-full">Choose</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Confirmation</DialogTitle>
+              <DialogDescription>
+                Are you sure with your choice? this action is can not be undone
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button
+                className="w-full"
+                onClick={onSubmit}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  <CheckCircle className="mr-2 h-4 w-4" />
+                )}{" "}
+                Yes, it is confirmed
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </CardFooter>
     </Card>
   );
